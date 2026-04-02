b8eafd0213c73b11983444e698e7fc3992e613fc
Your Name <your@email.com>
2026-04-02 11:08:38 +0530

    ex env

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ from dotenv import load_dotenv
 # Load .env file
 load_dotenv()
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_CHAT_ID = int(os.getenv("ADMIN_CHAT_ID"))
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+BOT_TOKEN = os.getenv("BOT_TOKEN_env")
+ADMIN_CHAT_ID = int(os.getenv("ADMIN_CHAT_ID_env"))
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY_env")
